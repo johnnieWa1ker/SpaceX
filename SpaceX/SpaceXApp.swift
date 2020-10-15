200066a9049c40a453fa283ea6f2c5d20fb1e703
Johnnie Walker <rockwave.fm@gmail.com>
2020-10-15 16:53:14 +0300

    Initial Commit

---
+++ SpaceX/SpaceXApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceXApp.swift
+//  SpaceX
+//
+//  Created by Johnnie Walker on 10/15/20.
+//
+
+import SwiftUI
+
+@main
+struct SpaceXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
